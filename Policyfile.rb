3e057e09322fb0d802caa59edf0ea7c02364793a
Chris Cunningham <therealchriscunningham@outlook.com>
2020-05-23 17:13:25 +0200

    add license and remove boilerplate from policyfile

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,17 +1,28 @@
-# Policyfile.rb - Describe how you want Chef Infra Client to build your system.
 #
-# For more information on the Policyfile feature, visit
-# https://docs.chef.io/policyfile.html
+# Author:: Chris Cunningham (<therealchriscunningham@outlook.com>)
+# Cookbook:: winget
+# Policy:: default
+#
+# Copyright:: 2020, Chris Cunningham
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# A name that describes what the system you're building with Chef does.
 name 'winget'
 
-# Where to find external cookbooks:
 default_source :supermarket
 
-# run_list: chef-client will run these recipes in the order specified.
 run_list 'test::default'
 
-# Specify a custom source for a single cookbook:
 cookbook 'winget', path: '.'
 cookbook 'test', path: 'test/cookbooks/test'
